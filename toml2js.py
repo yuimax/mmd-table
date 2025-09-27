8ea7ce799927f3da6cd170c3f769d33a7965b1d4
yuimax <yuimaroid@yahoo.co.jp>
2025-09-28 01:33:45 +0900

    commit 2025/09/28 01:33:44 (UST+9)

--- toml2js.py
+++ toml2js.py
@@ -6,21 +6,21 @@ toml_file = "mmdt.toml"
 with open(toml_file, 'rb') as f:
     data = tomllib.load(f)
 
-# item 配列と use 配列を取り出す
-item_list = data["item"]
-use_list = data["use"]
+# item 配列と uses 配列を取り出す
+item_list = data['item']
+uses_list = data['uses']
 
 # 配列 item_list を、辞書 item_dict に変換
 item_dict = {}
 for item in item_list:
-    item_id = item.pop("id")
+    item_id = item.pop('id')
     if item_id in item_dict:
         print(f"ERROR: ID \"{item_id}\" が重複しています")
         exit(1)
     item_dict[item_id] = item
-
-# item_dict と use_list をまとめてオブジェクト db を作る
-db = { "item": item_dict, "use": use_list }    
+    
+# item_dict と uses_list をまとめてオブジェクト db を作る
+db = { "item": item_dict, "uses": uses_list }    
 
 # オブジェクト db を JSON に変換する
 json_str = json.dumps(db, indent=2, ensure_ascii=False)
