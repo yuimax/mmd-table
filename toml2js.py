d264dad0578bb1db4646a2eb0e83b44237632f81
yuimax <yuimaroid@yahoo.co.jp>
2025-09-29 10:10:37 +0900

    commit 2025/09/29 10:10:36 (UST+9)

--- toml2js.py
+++ toml2js.py
@@ -3,17 +3,17 @@ import json
 
 # TOMLファイルを読み込む
 toml_file = "mmdt.toml"
-with open(toml_file, 'rb') as f:
+with open(toml_file, "rb") as f:
     data = tomllib.load(f)
 
 # item 配列と uses 配列を取り出す
-item_list = data['item']
-uses_list = data['uses']
+item_list = data["item"]
+uses_list = data["uses"]
 
 # 配列 item_list を、辞書 item_dict に変換
 item_dict = {}
 for item in item_list:
-    item_id = item.pop('id')
+    item_id = item.pop("id")
     if item_id in item_dict:
         print(f"ERROR: ID \"{item_id}\" が重複しています")
         exit(1)
@@ -27,7 +27,7 @@ json_str = json.dumps(db, indent=2, ensure_ascii=False)
 
 # JavaScript の代入文を作り、"mmdt.js" に書き込む
 js_file = "mmdt.js"
-with open(js_file, 'w', encoding='utf8') as f:
+with open(js_file, "w", encoding="utf8") as f:
     f.write(f"const db = {json_str};\n")
 
 print(f"CREATED: {js_file}")
